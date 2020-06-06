9a8067183fe589f41e30d706d122280759cf2dd4
Jack Quinoa <jquinoa208@gmail.com>
2020-06-06 20:25:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     anagrams = []
     sorted_word = word.split("").sort
     word_array.each do |array_word|
-      binding.pry
+  #binding.pry
       anagrams << array_word if array_word.split("").sort == sorted_word
     end
     anagrams
