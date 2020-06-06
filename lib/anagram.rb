4cabd81acba29602cbc6a809490f2f58caa93adc
Jack Quinoa <jquinoa208@gmail.com>
2020-06-06 20:11:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,19 @@
 require "pry"
-class Anagram 
+# class Anagram 
   
-  attr_accessor :word
+#   attr_accessor :word
   
-  def initialize(new_word)
-    @word = new_word
-  end
+#   def initialize(new_word)
+#     @word = new_word
+#   end
   
-  def match(word_array)
-  binding.pry
-    matches = []
-    word_array.each {|word| if word.split("").sort == @word.split("").sort 
-        return word
-    else 
-      return array_empty 
-    end }
-  end
-end
+#   def match(word_array)
+#   binding.pry
+#     matches = []
+#     word_array.each {|word| if word.split("").sort == @word.split("").sort 
+#         return word
+#     else 
+#       return array_empty 
+#     end }
+#   end
+# end
