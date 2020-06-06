5b96fdb2504e9534ebb12a46ef3cae6f06df0318
Jack Quinoa <jquinoa208@gmail.com>
2020-06-06 20:11:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,3 +17,25 @@ require "pry"
 #     end }
 #   end
 # end
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    anagrams =[]
+    sorted_word = word.split("").sort
+    array.each do |array_word|
+      #binding.pry
+      anagrams << array_word if array_word.split("").sort == sorted_word
+    end
+    anagrams
+
+  end
+
+
+end
